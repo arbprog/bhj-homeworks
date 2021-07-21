8c29bd475d820fa2b4afc6ab2e60000cfc826cfe
apbprog <baltabaevar3@gmail.com>
2021-07-21 21:43:26 +0300

    task "cookie-clicker" is done

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,25 +2,42 @@
 
 const clickerCounter = document.getElementById("clicker__counter");
 const image = document.getElementById("cookie");
-const clickerStatus = document.getElementById("clicker__status");
+const clickerSpeed = document.getElementById("clicker__speed__counter");
+const btn = document.getElementById("btn");
+let clickTime = 0;
 
-
+//Функция, которая считает клики
 function clickCounter() {
     clickerCounter.textContent = +clickerCounter.textContent + 1;
 }
 
-function changeSize() {
-    if(image.width === 200) {
-        image.width = 250;
+//Функция, которая считает скорость кликов
+function clickSpeedCounter() {   
+    let newClickTime = clickTime;
+    clickTime = new Date();
+    let result = 1000 / (clickTime - newClickTime);
+    
+    clickerSpeed.textContent = result.toFixed(2);
+}
+
+//Функция, которая изменяет размер картинки при клике
+function changeSize(element) {
+    if (element.target.width && element.target.width === 200) {
+        element.target.width += 20;
     } else {
-        image.width = 200;
+        element.target.width -= 20;
     }
 
     clickCounter();
+    clickSpeedCounter();
+}
+
+//Функция, которая очищает данные и картинку возвращает к первональному размеру
+function clear() {
+    clickerCounter.textContent = 0;
+    clickerSpeed.textContent = 0;
+    image.width = 200;
 }
 
 image.onclick = changeSize;
-
-
-
-
+btn.onclick = clear;
