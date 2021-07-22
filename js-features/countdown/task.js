de195e23b40a28656060e1a77ba49af56f415fe8
arbprog <baltabaevar3@gmail.com>
2021-07-22 10:22:07 +0300

    task 1.2 changed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,27 +1,27 @@
 "use strict";
 
-let time = 0;
 const timer = document.getElementById("timer");
 let deadline = prompt("Сколько часов осталость до конца конкурса?");
 
-if (deadline === null) {
+while (deadline === null || isNaN(deadline) !== false || deadline === "") {
     deadline = prompt("Сколько часов осталость до конца конкурса?");
 }
 
+let time = +deadline * 3600;
 
-// setInterval(updateTimer, 1000);
+setInterval(updateTimer, 1000);
 
-// function updateTimer() {
-//     let seconds = Math.floor(time % 60);
-//     let minutes = Math.floor(time / 60);
-//     let hours = Math.trunc(minutes / 60);   
+function updateTimer() {
+    let seconds = Math.floor(time % 60);
+    let minutes = Math.floor(time / 60);
+    let hours = Math.trunc(minutes / 60);   
 
-//     hours = hours < 10 ? `0${hours}` : hours;    
-//     minutes = minutes > 59 ? `${minutes % 60}` : minutes;
-//     minutes = minutes < 10 ? `0${minutes}` : minutes;
-//     seconds = seconds < 10 ? `0${seconds}` : seconds;    
+    hours = hours < 10 ? `0${hours}` : hours;    
+    minutes = minutes > 59 ? `${minutes % 60}` : minutes;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;    
 
-//     timer.textContent = `${hours}:${minutes}:${seconds}`;
+    timer.textContent = `${hours}:${minutes}:${seconds}`;
 
-//     time--;
-// }   
+    time--;
+}   
