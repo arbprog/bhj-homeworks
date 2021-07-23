6fd8e80e3de1f9c69cef1eefccb3a04a3d1fb4a5
apbprog <baltabaevar3@gmail.com>
2021-07-23 16:20:48 +0300

    task 1.3 changed

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const hole = document.getElementsByClassName("hole");
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+
+function getHole(index) {
+    
+}
