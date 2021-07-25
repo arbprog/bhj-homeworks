427d84641e9b356420bf059227bc157f58d27c9d
apbprog <baltabaevar3@gmail.com>
2021-07-25 17:05:01 +0300

    task 1.3 is done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,9 +1,37 @@
 "use strict";
 
-const hole = document.getElementsByClassName("hole");
+const elements = document.getElementsByClassName("hole");
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
 
-function getHole(index) {
-    
+function reload() {
+    dead.textContent = 0;
+    lost.textContent = 0;
 }
+
+function game() {
+    for (let i = 1; i <= elements.length; i++) {
+        let hole = document.getElementById(`hole${i}`);
+
+        hole.onclick = function() {
+
+            if (hole.classList.contains("hole_has-mole")) {
+                dead.textContent = +dead.textContent + 1;
+            } else {
+                lost.textContent = +lost.textContent + 1;
+            }
+        
+            if (dead.textContent === "10") {
+                alert(`Ура! Победа!`);
+                reload();
+            }
+
+            if (lost.textContent === "5") {
+                alert(`Вы проиграли! Попробуйте снова.`);
+                reload();
+            }
+        };
+    } 
+}
+
+game();
